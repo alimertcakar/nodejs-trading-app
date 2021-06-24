4a8055bb7cf88c475581f904ac83182d33cdbd2e
Ali Mert Çakar <mail@alimert.net>
2021-06-24 23:46:53 +0300

    code

--- models/Arama.mjs
+++ models/Arama.mjs
@@ -15,9 +15,10 @@ async function ara(term) {
     console.log(term)
     const _term = `%${term}%`
     try {
-
-        console.log(sql`SELECT * FROM listing WHERE title ILIKE ${_term}  OR description ILIKE ${_term}`)
-        const result = await pool.query(sql`SELECT * FROM listing WHERE title ILIKE ${_term}  OR description ILIKE ${_term} `);
+        const eslestirilenTerimlerRows = await pool.query(sql`select distinct terim,eslestirme from eslestirmeler WHERE terim ILIKE ${_term} `);
+        const similarityArr = "'%(" + eslestirilenTerimlerRows.map(row => row.eslestirme).map(val => val + "|").join("").slice(0, -1) + ")%'";
+        console.log(sql`SELECT * FROM listing WHERE title ILIKE ${_term}  OR description ILIKE ${_term} or lower(title) similar to  ${similarityArr}`)
+        const result = await pool.query(sql`SELECT * FROM listing WHERE title ILIKE ${_term}  OR description ILIKE ${_term} or lower(title) similar to  ${similarityArr} `);
         return result;
     }
     catch (e) {
